9e6235de06794e775ba1025d4e498e1e5a046a95
isama22 <elisabethmartinez@Elisabeths-MacBook-Pro.local>
2020-05-25 00:26:38 -0700

    added console.js, about to try implementing some preliminary login UI from the auth lesson starter code

--- models/user.js
+++ models/user.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
+
+const SALT_ROUNDS = 6;
 
 const userSchema = new mongoose.Schema({
     name: String,
@@ -13,4 +16,20 @@ const userSchema = new mongoose.Schema({
     timestamps: true
   });
 
+userSchema.pre('save', function(next){
+    const user = this;
+});
+
+userSchema.pre('save', function(next) {
+    const user = this;
+    if (!user.isModified('password')) return next();
+    // password has been changed - salt and hash it
+    bcrypt.hash(user.password, SALT_ROUNDS, function(err, hash) {
+      if (err) return next(err);
+      // replace the user provided password with the hash
+      user.password = hash;
+      next();
+    });
+  });
+  
 module.exports = mongoose.model('User', userSchema);
